964ed41c8a8bf26e97ff7a29caa1502b7f366c72
Josh leach <45842827+legoman101@users.noreply.github.com>
2020-06-12 14:57:58 +0100

    Update robotkeyboardcontrols.py

--- codes.py/robotkeyboardcontrols.py
+++ codes.py/robotkeyboardcontrols.py
@@ -31,57 +31,60 @@ def Backwards():
     GPIO.output(pinMotorABackwards, 1)
     GPIO.output(pinMotorBForwards, 0)
     GPIO.output(pinMotorBBackwards, 1)
- def Left():
+def Left():
     GPIO.output(pinMotorAForwards, 1)
     GPIO.output(pinMotorABackwards, 0)
     GPIO.output(pinMotorBForwards, 0)
     GPIO.output(pinMotorBBackwards, 1)
- def Right():
+def Right():
     GPIO.output(pinMotorAForwards, 0)
     GPIO.output(pinMotorABackwards, 1)
     GPIO.output(pinMotorBForwards, 1)
     GPIO.output(pinMotorBBackwards, 0)
- StopMotors()
+
 import sys, termios, tty, os
- def getch():
+def getch():
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
     try:
         tty.setraw(sys.stdin.fileno())
         ch = sys.stdin.read(1)
-     finally:
+    finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return ch
- PIN_LED = 25
+PIN_LED = 25
 GPIO.setup(PIN_LED, GPIO.OUT)
 GPIO.output(PIN_LED, 0)
 button_delay = 0.2
- for x in range(0,3):
+for x in range(0,3):
     GPIO.output(PIN_LED, 1)
     time.sleep(0.25)
     GPIO.output(PIN_LED, 0)
     time.sleep(0.25)
- while True:
+while True:
     char = getch()
-     if (char == "q"):
+    if (char == "q"):
         StopMotors()
         exit(0)  
-     if (char == "a"):
+    if (char == "a"):
         print 'Left pressed'
         Left()
         time.sleep(button_delay)
-     if (char == "d"):
+        StopMotors()
+    if (char == "d"):
         print 'Right pressed'
         Right()
         time.sleep(button_delay)          
-     elif (char == "w"):
+        StopMotors()
+    elif (char == "w"):
         print 'Up pressed' 
         Forwards()       
         time.sleep(button_delay)          
-    
+        StopMotors()
     elif (char == "s"):
         print 'Down pressed'      
         Backwards()
-        time.sleep(button_delay)  
+        time.sleep(button_delay)
+        StopMotors()
     
-    StopMotors()
+StopMotors()
