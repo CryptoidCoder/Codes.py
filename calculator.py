588f7630df5a2dd3977e6f74125c2af5c55c2a9f
Josh Leach <45842827+CryptoidCoder@users.noreply.github.com>
2022-01-08 14:01:57 +0000

    Updated calculator.py

--- calculator.py
+++ calculator.py
@@ -1,28 +1,29 @@
 # Python Program - Make Simple Calculator
 
-print("1. Addition");
-print("2. Subtraction");
-print("3. Multiplication");
-print("4. Division");
-print("5. Exit");
-choice = int(input("Enter your choice: "));
-if (choice>=1 and choice<=4):
-    print("Enter two numbers: ");
-    num1 = int(input());
-    num2 = int(input());
-    if choice == 1:
-    	res = num1 + num2;
-    	print("Result = ", res);
-    elif choice == 2:
-    	res = num1 - num2;
-    	print("Result = ", res);
-    elif choice == 3:
-    	res = num1 * num2;
-    	print("Result = ", res);
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+print("5. Exit")
+while True:
+    choice = None
+    choice = int(input("Which function? "))
+    if (choice>=1 and choice<=4):
+        num1 = int(input("First Number: "))
+        num2 = int(input("Second Number: "))
+        if choice == 1:
+            res = num1 + num2
+            print("Result = ", res)
+        elif choice == 2:
+            res = num1 - num2
+            print("Result = ", res)
+        elif choice == 3:
+            res = num1 * num2
+            print("Result = ", res)
+        else:
+            res = num1 / num2
+            print("Result = ", res)
+    elif choice == 5:
+        exit()
     else:
-    	res = num1 / num2;
-    	print("Result = ", res);
-elif choice == 5:
-    exit();
-else:
-    print("Wrong input..!!");
+        print("Wrong input..!!")
